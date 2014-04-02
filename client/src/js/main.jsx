edc95ff8709f0f37d9cf88b1200fe12a19e601b1
Magne Davidsen <magne.davidsen@gmail.com>
2014-04-02 22:28:47 +0200

    not getting pictures from db anymore

--- client/src/js/main.jsx
+++ client/src/js/main.jsx
@@ -2,199 +2,220 @@
 
 require.config({
     baseUrl: "/js/lib",
-    paths: { 
+    paths: {
         'react': ['//cdnjs.cloudflare.com/ajax/libs/react/0.9.0/react-with-addons.min', 'react/react'],
         'when': ['//cdnjs.cloudflare.com/ajax/libs/when/2.7.1/when.min', 'when/when']
     }
 });
 
-require(["rest/rest","rest/interceptor/mime", "rest/interceptor/errorCode", "react", "when"], function(rest,mime,errorCode, React, when) {
-	
-	function resetListView() {
-		rest('/api/Rappers').then(function(response) {
-			React.renderComponent(
-				<RapperList data={JSON.parse(response.entity)} listName="Norges beste rapper"/>,
-				document.getElementById('allRappers'));
-		});
+require(["rest/rest", "rest/interceptor/mime", "rest/interceptor/errorCode", "react", "when"], function (rest, mime, errorCode, React, when) {
 
-		rest('/api/Rappers/week').then(function(response) {
-			React.renderComponent(
-				<RapperList data={JSON.parse(response.entity)} listName="Ukens beste rapper"/>,
-				document.getElementById('weekRappers'));
-		});
-
-		rest('/api/Rappers/month').then(function(response) {
-			React.renderComponent(
-				<RapperList data={JSON.parse(response.entity)} listName="Månedens beste rapper"/>,
-				document.getElementById('monthRappers'));
-		});
-	}
-
-	function getTwoRandomRappers(callback) {
+    function getTwoRandomRappersPromise() {
         client = rest.chain(errorCode);
+        return client({ method: 'GET', path: "/api/Rappers/tworandom"})
+    }
 
-        client({ method: 'GET', path: "/api/Rappers/tworandom"}).then(callback,
-            function(response) {
-            console.error('getTwoRandomRappersError: ', JSON.stringify(response));
+    function getTwoRandomRappersAndThen(renderFunction) {
+
+        function renderTwoRandomRappers(response) {
+            //wait 500ms before rendering
             setTimeout(function () {
-                getTwoRandomRappers(callback);
+                renderFunction(response);
             }, 500);
+        }
+
+        function handleTwoRandomRappersError(response) {
+            console.error('getTwoRandomRappersError: ', JSON.stringify(response));
+            //wait 500ms before trying again
+            setTimeout(function () {
+                getTwoRandomRappersAndThen(renderFunction);
+            }, 1000);
+        }
+
+        //getTwoRandomRappers
+        var twoRandomPromise = getTwoRandomRappersPromise();
+
+        //whenNewRappersReturnRender, else handle error
+        twoRandomPromise.then(renderTwoRandomRappers, handleTwoRandomRappersError);
+    }
+
+    function resetListView() {
+        rest('/api/Rappers').then(function (response) {
+            React.renderComponent(
+                <RapperList data={JSON.parse(response.entity)} listName="Norges beste rapper"/>,
+                document.getElementById('allRappers'));
         });
-	}
 
-	function resetVoteView(response) {
-		React.renderComponent(
-			<VoteView data={JSON.parse(response.entity)}/>,
-			document.getElementById('voteView'));
-	}
+        rest('/api/Rappers/week').then(function (response) {
+            React.renderComponent(
+                <RapperList data={JSON.parse(response.entity)} listName="Ukens beste rapper"/>,
+                document.getElementById('weekRappers'));
+        });
 
-	var RapperListModule = React.createClass({
-		render: function() {
-			return (
-				<div>
+        rest('/api/Rappers/month').then(function (response) {
+            React.renderComponent(
+                <RapperList data={JSON.parse(response.entity)} listName="Månedens beste rapper"/>,
+                document.getElementById('monthRappers'));
+        });
+    }
+
+    function resetVoteView(response) {
+        React.renderComponent(
+            <VoteView data={JSON.parse(response.entity)} />,
+            document.getElementById('voteView'));
+    }
+
+    var RapperListModule = React.createClass({
+        render: function () {
+            return (
+                <div>
                     <div className="rapperLists" >
                         <div className="rapperListModule" id="allRappers" />
                         <div className="rapperListModule" id="monthRappers" />
                         <div className="rapperListModule" id="weekRappers" />
-				    </div>
+                    </div>
                     <div className="rapperLists" >
                         <div className="rapperListModule">
                             <div className="header">Tips?</div>
-                            <a href="http://twitter.com/hiphopograp" target="_blank"><div className="listItem" >@hiphopograp</div></a>
+                            <a href="http://twitter.com/hiphopograp" target="_blank">
+                                <div className="listItem" >@hiphopograp</div>
+                            </a>
                         </div>
                     </div>
                 </div>
 
-				);
-		}
-	});
+                );
+        }
+    });
 
-	var RapperList = React.createClass({
-		render: function() {
-			var rappers = this.props.data.map(function (rapper, i) {
-				return <div className="listItem">{i+1}. {rapper.name}</div>
-			});
-			return (
-				<div className="rapperList">
-					<div className="header">{this.props.listName}</div>
-					{rappers}
-				</div>
-				);
-		}
-	});
-
-	var RapperBox = React.createClass({
-		getInitialState: function() {
-   			return {voted: false};
-  		},
-
-		handleClick: function(event) {
-			var rapperSide = this.props.side;
-			var rapperBox = this;
-
-			ga('send', 'event', 'votebox', 'click', rapperSide);
-			rapperBox.props.updateReloading(true);
-
-			client = rest.chain(mime, { mime: 'application/json' }).chain(errorCode);
-			client({ method: 'POST', path: "/api/Vote", entity: {side:rapperSide} }).then(function(response) {
-				var twoRandomPromise = rest('/api/Rappers/tworandom');
-
-    			var wins = response.entity.wins;
-    			var losses = response.entity.losses;
-    			rapperBox.setState({voted:true, wins:wins, losses:losses});
-
-    			setTimeout(function(){
-    				console.log("hide view")
-    				twoRandomPromise.then(function(response){
-    					console.log("show view")
-    					rapperBox.props.updateReloading(false);
-    					rapperBox.setState({voted: false});
-    					resetVoteView(response);
-    				});
-    			} ,500);
-    		},function(response) {
-                console.error('vote error: ', JSON.stringify(response));
-                setTimeout(function () {
-                    console.log("show view")
-                    rapperBox.props.updateReloading(false);
-                    rapperBox.setState({voted: false});
-                    getTwoRandomRappers(resetVoteView);
-                }, 500);
+    var RapperList = React.createClass({
+        render: function () {
+            var rappers = this.props.data.map(function (rapper, i) {
+                return <div className="listItem">{i + 1}. {rapper.name}</div>
             });
-  		},
+            return (
+                <div className="rapperList">
+                    <div className="header">{this.props.listName}</div>
+					{rappers}
+                </div>
+                );
+        }
+    });
 
-		render: function() {
-			console.log("rendering rapperbox");
+    var RapperBox = React.createClass({
 
-			  var cx = React.addons.classSet;
-				var classes = cx({
-				    'rapperBox': true,
-                    'no-touch': !Modernizr.touch,
-				    'reloading': this.props.reloading
-				  });
+        getInitialState: function () {
+            return {voted: false};
+        },
 
-			var notVotedBox = (
-				<div className={classes} onClick={this.handleClick} >
-                    <img src={"data:" +this.props.picture.contentType + ";base64," + this.props.picture.data} />
-					<div className="rapperName">{this.props.rapperName}</div>
-				</div>
-				);
+        handleClick: function (event) {
+            var rapperSide = this.props.side;
+            var rapperBox = this;
 
-			var votedBox = (
-				<div className={classes}>
-                    <img src={"data:" +this.props.picture.contentType + ";base64," + this.props.picture.data} />
-					<div className="rapperName">{this.props.rapperName}</div>					
-				</div>
-				);
+            function handleVotingFailed(response) {
+                console.error('vote error: ', JSON.stringify(response));
+            }
 
-			return this.state.voted ? votedBox : notVotedBox;
-		}
-	});
+            function whenPicturesAreLoadedfadeInAndMakeClickable(response) {
+                console.log("show view");
+
+                rapperBox.props.updateReloading(false);
+                rapperBox.setState({voted: false});
+
+                resetVoteView(response);
+            }
+
+            ga('send', 'event', 'votebox', 'click', rapperSide);
+
+            //fadeOut
+            rapperBox.props.updateReloading(true);
+
+            //makeBoxes unclickable
+            rapperBox.setState({voted: true})
+
+            //sendVote
+            client = rest.chain(mime, { mime: 'application/json' }).chain(errorCode);
+            var votePromise = client({ method: 'POST', path: "/api/Vote", entity: {side: rapperSide}});
+
+            //voteReturns
+            votePromise.then(function (response) {
+                console.log("voted")
+            }, handleVotingFailed);
+
+            getTwoRandomRappersAndThen(whenPicturesAreLoadedfadeInAndMakeClickable);
+        },
+
+        render: function () {
+            console.log("rendering rapperbox");
+
+            var cx = React.addons.classSet;
+            var classes = cx({
+                'rapperBox': true,
+                'no-touch': !Modernizr.touch,
+                'reloading': this.props.reloading
+            });
+
+            return (
+                <div className={classes} onClick={this.state.voted ? "" : this.handleClick} >
+                    <img id={this.props.side + "pic"} src={"pictures/" + this.props.fileName} />
+                    <div className="rapperName">{this.props.rapperName}</div>
+                </div>
+                );
+        }
+    });
 
 
-	var RappersView = React.createClass({
-		getInitialState: function() {
-   			return {reloading: false};
-  		},
+    var RappersView = React.createClass({
+        getInitialState: function () {
+            return {reloading: false};
+        },
 
-  		setReloading: function(value) {
-  			this.setState({reloading: value});
-  		},
+        setReloading: function (value) {
+            this.setState({reloading: value});
+        },
 
-		render: function() {
-			var leftRapper = this.props.data.left;
-			var rightRapper = this.props.data.right;
-				
-			return (
-    				<div className="voteBox">
-					<RapperBox updateReloading={this.setReloading} reloading={this.state.reloading} picture={leftRapper.picture} rapperName={leftRapper.name} side="left" />
-					<RapperBox updateReloading={this.setReloading} reloading={this.state.reloading} picture={rightRapper.picture} rapperName={rightRapper.name} side="right"  />
-				</div>
-				);
-		}
-	});
+        render: function () {
+            var leftRapper = this.props.data.left;
+            var rightRapper = this.props.data.right;
 
-	var VoteView = React.createClass({
-		render: function() {
-			return (
-				<div className="voteView">
+            return (
+                <div className="voteBox">
+                    <RapperBox updateReloading={this.setReloading} reloading={this.state.reloading} rapperName={leftRapper.name}
+                    fileName={leftRapper.picture.fileName} side="left" />
+                    <RapperBox updateReloading={this.setReloading} reloading={this.state.reloading} rapperName={rightRapper.name}
+                    fileName={rightRapper.picture.fileName} side="right"  />
+                </div>
+                );
+        }
+    });
+
+    var VoteView = React.createClass({
+        render: function () {
+            return (
+                <div className="voteView">
                     <img className="nbrnoimg" src="styles/nbrlogo.png"/>
                     <img className="nbrnoimg" src="styles/nbr.png"/>
                     <img className="nbrnoimg" src="styles/nbrlogo.png"/>
                     <h2>Hvem er best?</h2>
 
+                    <RappersView data={this.props.data} />
 
-				<RappersView data={this.props.data} />
+                </div>
+                );
+        }
+    });
 
-				</div>
-				);
-		}
-	});
+    React.renderComponent(
+        <RapperListModule />,
+        document.getElementById('listView'));
 
-	React.renderComponent(
-			<RapperListModule />,
-			document.getElementById('listView'));
-
-	getTwoRandomRappers(resetVoteView);
-	resetListView();
-});
+    getTwoRandomRappersPromise().then(resetVoteView, function (response) {
+        console.error('getTwoRandomRappersError: ', JSON.stringify(response));
+        //wait 500ms before trying again
+        setTimeout(function () {
+            getTwoRandomRappersAndThen(resetVoteView);
+        }, 1000);
+    });
+    resetListView();
+})
+;
