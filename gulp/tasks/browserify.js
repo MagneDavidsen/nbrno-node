dfd853432412434a005eafdb1348d93363e278a0
Magne Davidsen <magne.davidsen@gmail.com>
2014-09-29 09:10:50 +0200

    minify js

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -2,14 +2,19 @@ var browserify   = require('browserify');
 var gulp         = require('gulp');
 var handleErrors = require('../util/handleErrors');
 var source       = require('vinyl-source-stream');
+var buffer 			 = require('vinyl-buffer');
+var uglify 			 = require('gulp-uglify');
+
 
 gulp.task('browserify', function(){
 	return browserify({
 			entries: ['./src/js/main.jsx'],
 			extensions: ['.js']
 		})
-		.bundle({debug: true})
+		.bundle({debug: false})
 		.on('error', handleErrors)
 		.pipe(source('bundle.js'))
+		.pipe(buffer())
+		.pipe(uglify())
 		.pipe(gulp.dest('./dist/js'));
 });
